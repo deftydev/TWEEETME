e38c49bb0b1986e02be3f44f38369b47bbe7ef44
deftydev <devanshgupta79212346@gmail.com>
2020-06-17 01:12:42 +0530

    INITIAL COMMIT

---
+++ profiles/api/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from .views import user_follow_view,profile_detail_api_view
+
+urlpatterns = [
+    path('<str:username>/follow/', user_follow_view),
+    path('<str:username>/', profile_detail_api_view),
+]
---
+++ profiles/models.py
@@ -0,0 +1,24 @@
+from django.conf import settings
+from django.db import models
+from django.db.models.signals import post_save
+
+User=settings.AUTH_USER_MODEL
+
+class FollowerRelation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    profile = models.ForeignKey("Profile", on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+class Profile(models.Model):
+    user=models.OneToOneField(User, on_delete=models.CASCADE)
+    location=models.CharField(max_length=120,null=True,blank=True)
+    bio = models.TextField(blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    followers = models.ManyToManyField(User,related_name='following',blank=True)
+
+def user_did_save(sender,instance,created,*args,**kwargs):
+    if created:
+        Profile.objects.get_or_create(user=instance)
+
+post_save.connect(user_did_save, sender=User)
